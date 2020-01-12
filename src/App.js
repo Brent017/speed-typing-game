e77264a983e391e06dfc1a33fc60919e19821248
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2020-01-12 08:38:30 -0700

    text handleChange

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
+  console.log(text);
+
   return (
     <div>
       <h1>Speed Typing Challenge</h1>
-      <textarea />
+      <textarea onChange={handleChange} value={text} />
       <h4>Time remaining: ??? seconds</h4>
       <button>Start</button>
       <h1>Word Count: ??? words</h1>
